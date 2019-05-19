96560ed53c5df42fc732651e3c5e1468987a05f0
matheusbuldrini <38816666+matheusbuldrini@users.noreply.github.com>
2019-05-18 22:20:46 -0300

    Quarto COMMIT & PUSH

--- src/tdd/Franc.java
+++ src/tdd/Franc.java
@@ -1,12 +1,12 @@
 package tdd;
 
 class Franc extends Money{   
-				
-	   Franc(int amount) {      
-	      this.amount= amount;
-	   }					
+	
+	   Franc(int amount, String currency) {
+	          super(amount, currency);
+	   }			
 	   	
 	   Money times(int multiplier)  {
-		      return new Franc(amount * multiplier);
+		      return Money.franc(amount * multiplier);
 	   }
 }
