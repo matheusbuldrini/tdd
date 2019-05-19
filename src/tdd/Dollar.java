171ec3c20a9d6b255f580e105a17504b61e95a32
matheusbuldrini <38816666+matheusbuldrini@users.noreply.github.com>
2019-05-18 22:09:56 -0300

    Terceiro COMMIT & PUSH

--- src/tdd/Dollar.java
+++ src/tdd/Dollar.java
@@ -5,8 +5,8 @@ class Dollar extends Money {
 	   Dollar(int amount) {
 	      this.amount= amount;
 	   }
-	   Dollar times(int multiplier) {
-	      return new Dollar(amount * multiplier);
-	   }
 	   
+	   Money times(int multiplier)  {
+		      return new Dollar(amount * multiplier);
+	   } 
 }
--- src/tdd/Franc.java
+++ src/tdd/Franc.java
@@ -4,8 +4,9 @@ class Franc extends Money{
 				
 	   Franc(int amount) {      
 	      this.amount= amount;
-	    }					
-	    Franc times(int multiplier)  {      
-	       return new Franc(amount * multiplier);					
-	    }				
-	}
+	   }					
+	   	
+	   Money times(int multiplier)  {
+		      return new Franc(amount * multiplier);
+	   }
+}
